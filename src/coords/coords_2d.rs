166e85cc3332d17d9db71e3bf243fa874f0dbce7
AnsonH <ansonheung2000@gmail.com>
2025-12-13 17:00:59 +0800

    Refactor coords module

---
+++ src/coords/coords_2d.rs
@@ -0,0 +1,22 @@
+use std::fmt::Debug;
+
+/// Represents a 2D coordinate.
+#[derive(Clone, Copy, Default, PartialEq, Eq, Hash)]
+pub struct Coords2D {
+    pub x: i64,
+    pub y: i64,
+}
+
+impl Coords2D {
+    #[must_use]
+    #[inline]
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Debug for Coords2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Coords2D({}, {})", self.x, self.y)
+    }
+}
---
+++ src/coords/mod.rs
@@ -0,0 +1,5 @@
+pub mod coords_2d;
+pub mod coords_3d;
+
+pub use coords_2d::*;
+pub use coords_3d::*;
